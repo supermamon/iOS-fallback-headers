800452c08fbd3daaa50c4ce714645a69d44fe18a
supermamon <monvelasquez@gmail.com>
2016-03-06 16:19:24 +0800

    Added missing methods to PSSpecifier

---
+++ Preferences/PSSpecifier+Missing.h
@@ -0,0 +1,4 @@
+@interface PSSpecifier (Missing)
+-(void)setValues:(NSArray*)values titles:(NSArray*)titles;
+-(void)setValues:(NSArray*)values titles:(NSArray*)titles shortTitles:(NSArray*)shortTitles;
+@end
